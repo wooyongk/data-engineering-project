25809cc42de8d5f68165cbcfd2ac80b07547324a
wooyong <wooyong.kim@kr.doubledown.com>
2025-01-13 12:01:20 +0900

    HolidayOperator 추가

--- airflow/dags/custom_operators/README.md
+++ airflow/dags/custom_operators/README.md
@@ -1,7 +1,10 @@
 ## SQLColumnValueExistenceCheckOperator
 
----
-
 `SQLColumnValueExistenceCheckOperator` 는 Airflow의 `BaseSQLOperator`를 확장한 사용자 정의 연산자로,
 특정 SQL 테이블의 지정된 열에 특정 값들이 존재하는지 확인하는 기능을 제공합니다.
 연산자는 주어진 테이블과 열 이름, 그리고 확인할 값들을 매개변수로 받아, 각각의 값이 해당 열에 존재하는지 여부를 검사합니다.
+
+## HolidayOperator
+
+`HolidayOperator`는 Airflow에서 데이터 데이터베이스에 저장된 휴일 정보를 기반으로 DAG의 실행 흐름을 동적으로 제어하는 Custom Operator입니다.
+휴일 정보에 따라 특정 태스크를 실행하거나 건너뛸 수 있어 유연한 스케줄 관리를 지원합니다.
--- airflow/dags/custom_operators/sql.py
+++ airflow/dags/custom_operators/sql.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 
+from typing import Any
 from typing import List
 from typing import Sequence
 
 from airflow.exceptions import AirflowException
+from airflow.models.skipmixin import SkipMixin
 from airflow.providers.common.sql.operators.sql import BaseSQLOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
+from airflow.utils.context import Context
 
 
 class SQLColumnValueExistenceCheckOperator(BaseSQLOperator):
@@ -54,3 +58,55 @@ class SQLColumnValueExistenceCheckOperator(BaseSQLOperator):
 
         self.log.info("Check results: %s", value_existence_results)
         return value_existence_results
+
+
+class HolidayOperator(BaseSQLOperator, SkipMixin):
+    def __init__(
+        self,
+        *,
+        mysql_conn_id: str = "MYSQL_DATABASE_DATA",
+        country: str = "KR",
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.mysql_conn_id = mysql_conn_id
+        self.country = country
+
+    def execute(self, context: Context) -> Any:
+        mysql_hook = MySqlHook(mysql_conn_id=self.mysql_conn_id)
+
+        query = "SELECT open_yn FROM holiday WHERE country_code = %s AND date = %s"
+        query_date = (context["data_interval_end"].in_timezone("Asia/Seoul")).date()
+        self.log.info(
+            "Executing query for country %s on date %s.", self.country, query_date
+        )
+
+        records = mysql_hook.get_records(query, parameters=(self.country, query_date))
+
+        if not records:
+            self.log.warning(
+                "No records returned from the query. Skipping downstream tasks."
+            )
+            self._skip_downstream_tasks(context)
+            return
+
+        result = records[0][0]
+        self.log.info("Result for column 'open_yn': %s", result)
+
+        if result and result.upper() == "Y":
+            self.log.info("Proceeding with downstream tasks.")
+        else:
+            self.log.info("Skipping downstream tasks because 'open_yn' is not 'Y'.")
+            self._skip_downstream_tasks(context)
+
+    def _skip_downstream_tasks(self, context: Context) -> None:
+        """Helper method to skip downstream tasks."""
+        downstream_tasks = context["task"].get_flat_relatives(upstream=False)
+        if not downstream_tasks:
+            self.log.info("No downstream tasks to skip.")
+            return
+
+        self.log.info(
+            "Skipping downstream tasks: %s", [t.task_id for t in downstream_tasks]
+        )
+        self.skip(context["dag_run"], context["logical_date"], downstream_tasks)
