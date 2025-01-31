b265d02af6eea3962ceeb92d996b7fd110aa65cd
wooyong <wooyong.kim@kr.doubledown.com>
2025-01-31 17:18:59 +0900

    재시도 횟수 및 시간 수정

--- airflow/dags/common/default_config.py
+++ airflow/dags/common/default_config.py
@@ -5,8 +5,8 @@ from common.telegram_notification import on_failure
 default_dag_config = {
     "owner": "engineer-team",
     "depends_on_past": False,
-    "retries": 0,
-    "retry_delay": datetime.timedelta(seconds=15),
+    "retries": 3,
+    "retry_delay": datetime.timedelta(seconds=30),
     "on_failure_callback": on_failure,
     # "on_success_callback": ,
 }
